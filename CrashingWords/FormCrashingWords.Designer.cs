dcfa2ef7c9048a18e409d8c774fa688f558af100
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-18 17:01:11 +0200

    Add project files.

---
+++ CrashingWords/FormCrashingWords.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CrashingWords
+{
+    partial class FormCrashingWords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCrashingWords = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCrashingWords
+            // 
+            this.BtnCrashingWords.Location = new System.Drawing.Point(47, 36);
+            this.BtnCrashingWords.Name = "BtnCrashingWords";
+            this.BtnCrashingWords.Size = new System.Drawing.Size(186, 61);
+            this.BtnCrashingWords.TabIndex = 0;
+            this.BtnCrashingWords.Text = "Crashing Words";
+            this.BtnCrashingWords.UseVisualStyleBackColor = true;
+            this.BtnCrashingWords.Click += new System.EventHandler(this.BtnCrashingWords_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(341, 36);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(403, 330);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormCrashingWords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCrashingWords);
+            this.Name = "FormCrashingWords";
+            this.Text = "Crashing Words";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCrashingWords;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ CrashingWords/FormCrashingWords.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CrashingWords
+{
+    public partial class FormCrashingWords : Form
+    {
+        public FormCrashingWords()
+        {
+            InitializeComponent();
+        }
+
+
+        int crashingIndexPos;
+        int lenght;
+
+        private void BtnCrashingWords_Click(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + @"\words.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            string line1 = "";
+            string line2 = "";
+
+
+            while (true)
+            {
+                line1 = streamReader.ReadLine();
+                line2 = streamReader.ReadLine();
+
+                if (line1 == null || line2 == null)
+                    break;
+
+                string[,] words = new string[line1.Length, line1.Length];
+
+                if (IsCrashingWords(line1, line2))
+                {
+                    lenght = line1.Length;
+                    string firstWord = CheckWhichFirst(line1, line2);
+
+                    if (firstWord == line1)
+                    {
+                        words = LoadWords(line1, line2);
+                    }
+                    else
+                    {
+                        words = LoadWords(line2, line1);
+                    }
+                }
+
+                Display(words);
+            }
+
+        }
+
+        private bool IsCrashingWords(string word1, string word2)
+        {
+            //The two words are always the same lenght as per the assignment.
+            for (int i = 0; i < word1.Length; i++)
+            {
+                if(word1.Substring(i, 1) == word2.Substring(i, 1))
+                {
+                    crashingIndexPos = i;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private string CheckWhichFirst(string word1, string word2)
+        {
+            int posWord1 = 0;
+            int posWord2 = 0;
+
+            string[] alphabet = new string[27]
+                { "", "A", "B", "C", "D", "E", "F", "G", "I", "H", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+                "X", "Y", "Z" };
+
+            for (int i = 1; i <= 26; i++)
+            {
+                if (word1.Substring(0, 1) == alphabet[i])
+                {
+                    posWord1 = i;
+                }
+
+                if (word2.Substring(0, 1) == alphabet[i])
+                {
+                    posWord2 = i;
+                }
+            }
+
+            if (posWord1 < posWord2)
+            {
+                return word1;
+            }
+            else
+            {
+                return word2;
+            }
+        }
+
+        private string[,] LoadWords(string word1, string word2)
+        {
+            string[,] display = new string[word1.Length, word1.Length];
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                for (int j = 0; j < word1.Length; j++)
+                {
+                    if (j == crashingIndexPos)
+                    {
+                        display[i, j] = word1.Substring(i, 1);
+                    }
+                    else
+                    {
+                        display[i, j] = " ";
+                    }
+                }
+            }
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                for (int j = 0; j < word1.Length; j++)
+                {
+                    if (i == crashingIndexPos)
+                    {
+                        display[i, j] = word2.Substring(j, 1);
+                    }
+                }
+            }
+            return display;
+        }
+
+        private void Display(string[,] words)
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < lenght; j++)
+                {
+                    TxtResult.Text += words[i, j];
+                }
+            }
+        }
+    }
+}
